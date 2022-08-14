b905e0b50d9c36fd8bedcf392b67df0fb6fc0527
Sanjay Dutta <sanjay.aug.02.1998@gmail.com>
2022-08-14 18:44:10 +0530

    Added few lines

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
     <h1>{{ message }}</h1>
+    <h2>Enter your Name: <input type="text" v-model=name></h2>
+    <button @click="changeName">Click to change name</button>
+      <h1 v-if="buttonClicked">{{ inputName }}</h1>
   </div>
 </template>
 
@@ -9,7 +12,16 @@ export default {
   name: 'IndexPage',
   data() {
     return {
-      message: 'Hello Sanjay'
+      message: 'Hello World',
+      name:"",
+      inputName:"",
+      buttonClicked:true
+    }
+  },
+  methods: {
+    changeName() {
+      this.inputName = `Hello ${this.name}`
+     //this.buttonClicked = !this.buttonClicked
     }
   }
 }
